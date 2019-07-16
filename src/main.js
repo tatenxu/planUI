f9642dd2e376a83d169a0972e60fbab5a560713b
zcompzhe <314516682@qq.com>
2019-07-16 15:15:56 +0800

    qwe

--- src/main.js
+++ src/main.js
@@ -69,16 +69,16 @@ window.$config = {};
 
 //dev
 
-window.$config.HOST = 'http://192.168.1.180:8080'
-window.$config.HOST2 = 'http://192.168.1.180:8081/uaa';
+// window.$config.HOST = 'http://192.168.1.180:8080'
+// window.$config.HOST2 = 'http://192.168.1.180:8081/uaa';
 
 //dev
 // window.$config.HOST = 'http://192.168.1.159:8080'
 // window.$config.HOST2 = 'http://192.168.1.159:8080/uaa';
 
 // production
-// window.$config.HOST = 'http://202.120.1.66:8080/planservice'
-// window.$config.HOST2 = 'http://202.120.1.66:8080/service-authorization/uaa';
+window.$config.HOST = 'http://202.120.1.66:8080/planservice'
+window.$config.HOST2 = 'http://202.120.1.66:8080/service-authorization/uaa';
 
 /* eslint-disable no-new */
 new Vue({
